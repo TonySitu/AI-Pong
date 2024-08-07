5407fc1edfa9bbd46a3e92219b64561ee7508168
Tony <137113805+TonySitu@users.noreply.github.com>
2024-08-06 23:56:55 -0400

    created PongGame class

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from pong import ball
 from pong import paddle
 
 
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = game.GameModel(window, width, height)
+        self.ball = self.game.ball
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+
+
 def main():
     pass
 
